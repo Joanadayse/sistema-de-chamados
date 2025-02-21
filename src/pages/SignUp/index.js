6f224f30d1af67b8f666af8c7e63ef78e3b394c3
Joanadayse <daysejoana3@gmail.com>
2025-02-21 11:40:39 -0300

    criando a pagina de cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,3 +1,43 @@
+import { useState } from "react";
+import logo from "../../assests/logo.png";
+import { Link } from "react-router-dom";
+
 export default function SignUp() {
-  return <h1>Pagina de Cadastro</h1>;
+  const [name,setName]= useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="logo" />
+        </div>
+
+        <form>
+          <h1>Nova Conta</h1>
+          <input
+            type="text"
+            placeholder="Seu Nome"
+            value={name}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="*****"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type="submit">Cadastrar</button>
+        </form>
+        <Link to="/"> Já possui uma conta? Faça Login</Link>
+      </div>
+    </div>
+  );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { Route, Routes,  } from "react-router-dom";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import Dashboard from "../pages/Dashboard";
 
 
 
@@ -9,6 +10,7 @@ function RoutesApp(){
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/register" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard/>}/>
       </Routes>
     );
 }
