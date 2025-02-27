e70e055c4b0f966e2268adb3a46b82d149900947
Joanadayse <daysejoana3@gmail.com>
2025-02-27 16:11:45 -0300

    cadastrando clientes

--- src/pages/Custumers/index.js
+++ src/pages/Custumers/index.js
@@ -2,56 +2,76 @@ import { FiUser } from "react-icons/fi";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 import { useState } from "react";
+import { db } from "../../services/firebaseConection";
+import { addDoc, collection } from "firebase/firestore";
+import { toast } from "react-toastify";
 
-export default function Custumers (){
+export default function Custumers() {
+  const [nome, setNome] = useState("");
+  const [cnpj, setCnpj] = useState("");
+  const [endereco, setEndereco] = useState("");
 
-    const [nome , setNome]= useState("");
-        const [cnpj, setCnpj] = useState("");
-            const [endereco, setEndereco] = useState("");
+  async function handlerRegister(e) {
+    e.preventDefault();
+    if (nome !== "" && cnpj !== "" && endereco !== "") {
+      await addDoc(collection(db, "custumers"), {
+        nomeFantasia: nome,
+        cnpj: cnpj,
+        endereco: endereco,
+      })
+        .then(() => {
+          setNome("");
+          setCnpj("");
+          setEndereco("");
+          toast.success("Empresa Registrada !");
+        })
+        .catch((error) => {
+          console.log(error);
+          toast.error("erro ao fazer o cadastro !");
+        });
+    } else {
+      toast.error("Preencha todos os campos !");
+    }
+  }
+  return (
+    <div>
+      <Header />
 
-            function handlerRegister(e){
-                e.preventDefault();
-                alert("Funcionou!")
-            }
-    return (
-      <div>
-        <Header />
+      <div className="content">
+        <Title name="Clientes">
+          <FiUser size={25} />
+        </Title>
 
-        <div className="content">
-          <Title name="Clientes">
-            <FiUser size={25} />
-          </Title>
+        <div className="container">
+          <form className="form-profile" onSubmit={handlerRegister}>
+            <label>Nome fantasia</label>
+            <input
+              type="text"
+              placeholder="Nome da empresa"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+            />
 
-          <div className="container">
-            <form className="form-profile" onSubmit={handlerRegister}>
-              <label>Nome fantasia</label>
-              <input
-                type="text"
-                placeholder="Nome da empresa"
-                value={nome}
-                onChange={(e) => setNome(e.target.value)}
-              />
+            <label>CNPJ</label>
+            <input
+              type="text"
+              placeholder="Digite o Cnpj"
+              value={cnpj}
+              onChange={(e) => setCnpj(e.target.value)}
+            />
 
-              <label>CNPJ</label>
-              <input
-                type="text"
-                placeholder="Digite o Cnpj"
-                value={cnpj}
-                onChange={(e) => setCnpj(e.target.value)}
-              />
+            <label>Endereço</label>
+            <input
+              type="text"
+              placeholder="Endereço da empresa"
+              value={endereco}
+              onChange={(e) => setEndereco(e.target.value)}
+            />
 
-              <label>Endereço</label>
-              <input
-                type="text"
-                placeholder="Endereço da empresa"
-                value={endereco}
-                onChange={(e) => setEndereco(e.target.value)}
-              />
-
-              <button type="submit"> Salvar</button>
-            </form>
-          </div>
+            <button type="submit"> Salvar</button>
+          </form>
         </div>
       </div>
-    );
+    </div>
+  );
 }
