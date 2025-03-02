81593ac1aaf6159ac30789facd2d3a9c854c3123
Joanadayse <daysejoana3@gmail.com>
2025-03-02 17:02:31 -0300

    buscando chamados parte 2

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -7,6 +7,7 @@ import { FiPlus, FiMessageSquare , FiSearch, FiEdit2 } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
 import { db } from "../../services/firebaseConection";
+import { format } from "date-fns";
 
 
 const listRef= collection(db,"chamados");
@@ -24,6 +25,10 @@ export default function Dashboard() {
 
     const q = query(listRef, orderBy("created", "desc"), limit(5));
     const querySnapshot = await getDocs(q)
+    // para não ocasionar a duplicidade -React.StrictMode- em desenvolvimento;
+    setChamados([]) 
+
+    // buscar chamados
     await updateState(querySnapshot)
     setLoading(false);
     
@@ -32,7 +37,7 @@ export default function Dashboard() {
    loadChamados();
 
    return()=>{}
-  })
+  }, []);
 
 
 // manipular a lista e colocar dentro do usestate , percorre a lista de chamados;
@@ -47,6 +52,7 @@ export default function Dashboard() {
           cliente:doc.data().cliente,
           clienteId:doc.data().clienteId,
           created: doc.data().created,
+          createdFormat: format(doc.data().created.toDate(), "dd/MM/yyyy"),
           status:doc.data().status,
           complemento: doc.data().complemento,
 
@@ -61,6 +67,26 @@ export default function Dashboard() {
 
     }
     
+  }
+
+  // antes de buscar os chamados - setLoading(true), ele renderiza "Buscando chamados..." , apos carregar todos os chamados  setLoading(false) , renderiza toda a tabela com os chamados;
+
+  if(loading){
+    return (
+      <div>
+        <Header />
+
+        <div className="content">
+          <Title name="Tickets">
+            <FiMessageSquare size={25} />
+          </Title>
+
+          <div className="container dashboard">
+            <span>Buscando chamados...</span>
+          </div>
+        </div>
+      </div>
+    );
   }
 
 
@@ -104,39 +130,45 @@ export default function Dashboard() {
                     <th scope="col">Assunto</th>
                     <th scope="col">Status </th>
                     <th scope="col">Cadastrado em</th>
-                    <th scope="col">*</th>
+                    <th scope="col">#</th>
                   </tr>
                 </thead>
 
                 <tbody>
-                  <tr>
-                    <td data-label="Clientes">Mercado esquina</td>
-                    <td data-label="Assunto">Suporte</td>
-                    <td data-label="Status">
-                      <span
-                        className="badge"
-                        style={{ backgroundColor: "#999" }}
-                      >
-                        Em aberto
-                      </span>
-                    </td>
-                    <td data-label="Cadastrado">27/02/25</td>
-                    <td data-label="#">
-                      <button
-                        className="action"
-                        style={{ backgroundColor: "#3583f6" }}
-                      >
-                        <FiSearch color="#fff" size={17} />
-                      </button>
+                  {
+                    chamados.map((item , index)=>{
+                      return (
+                        <tr key={index}>
+                          <td data-label="Clientes">{item.cliente}</td>
+                          <td data-label="Assunto">{item.assunto}</td>
+                          <td data-label="Status">
+                            <span
+                              className="badge"
+                              style={{ backgroundColor: "#999" }}
+                            >
+                              {item.status}
+                            </span>
+                          </td>
+                          <td data-label="Cadastrado">{item.createdFormat}</td>
+                          <td data-label="#">
+                            <button
+                              className="action"
+                              style={{ backgroundColor: "#3583f6" }}
+                            >
+                              <FiSearch color="#fff" size={17} />
+                            </button>
 
-                      <button
-                        className="action"
-                        style={{ backgroundColor: "#f6a935" }}
-                      >
-                        <FiEdit2 color="#fff" size={17} />
-                      </button>
-                    </td>
-                  </tr>
+                            <button
+                              className="action"
+                              style={{ backgroundColor: "#f6a935" }}
+                            >
+                              <FiEdit2 color="#fff" size={17} />
+                            </button>
+                          </td>
+                        </tr>
+                      );
+                    })
+                  }
                 </tbody>
               </table>
             </>
